13ffcde9383d038001f879277f6abb30f68b5c00
faas001 <42976743+faas001@users.noreply.github.com>
2018-11-16 05:52:26 -0500

    Error handling started
    Need to find out  about errors being displayed in the node server console. Doesn't appear to visually effect web page functionality.

--- app.js
+++ app.js
@@ -2,30 +2,39 @@ const express = require('express');
 
 const data = require('./data.json');
 const projects = data.projects;
-console.log(projects);
+
 const app = express();
 
 app.set('view engine', 'pug');
 app.use('/static', express.static('public'));
 
-app.get('/', (req, res) => {
-   console.log('root');
-        res.render('index', { projects });
-    
+app.get('/', (req, res, next) => {
+    console.log('root');
+    res.render('index', { projects });
+    next();
 });
 
-app.get('/about', (req, res) => {
-    
+app.get('/about', (req, res, next) => {
     res.render('about');
-
+    next();
 });
 
-app.get('/project_:id', (req, res) => {
-    
+app.get('/project_:id', (req, res, next) => { 
     res.render('project', { projects, id: req.params.id });
-
+    next();
 });
 
+app.use((req, res, next) => {
+    const err = new Error('Page not found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error', err);
+});
 
 
 app.listen(3000, () => {
